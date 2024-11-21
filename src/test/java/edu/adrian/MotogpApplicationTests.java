f7586bfcdc5b6781252f1bdd7b20b36680a3ad4e
Adrián Canedo <23adricanebello@gmail.com>
2024-11-21 12:38:44 +0100

    inicio proyecto motogp

---
+++ src/test/java/edu/adrian/MotogpApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.adrian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotogpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
